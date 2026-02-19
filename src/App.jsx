78cc6fbb514e979dabb3ec46479057da69e6e7dd
Taviquenson <tavo2070@gmail.com>
2026-02-19 12:26:31 -0800

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react'
 
 export default function App() {
+  
 
   return (
     <>
-      <h1>Hello, world!</h1>
+      <img className='blob-top' src='../public/blob-top-home.png'/>
+      <section className='start'>
+        <h1>Quizzical</h1>
+        <h2>A React trivia app</h2>
+        <button>Start quiz</button>
+      </section>
+      <img className='blob-bottom' src='../public/blob-bottom-home.png'/>
     </>
   )
 }
